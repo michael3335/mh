f947bf404b2b9373f2635deb3eeb0f670568b2d4
michael3335 <mickyharr@gmail.com>
2025-11-07 19:33:26 +1100

    added bot id

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
+import { withBotId } from "botid/next/config";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typedRoutes: true,
 };
 
-export default nextConfig;
+export default withBotId(nextConfig);
